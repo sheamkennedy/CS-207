abe1cefc237b1c1aa618292d97072f4240ec1bf0
sheamkennedy <sheamkennedy@gmail.com>
2015-10-28 15:10:30 -0600

    Update Synth.ino
    Added comments at the top of every function I have created to giver user a general idea of what happens in each chunk of code.

--- Synth.ino
+++ Synth.ino
@@ -4,9 +4,7 @@
   Example using a potentiometer to control the amplitude of a sinewave
   with Mozzi sonification library. This example has been further modified
   to support more user input control. 
-
   Demonstrates the use of Oscil to play a wavetable, and analog input for control.
-
   This example goes with a tutorial on the Mozzi site:
   http://sensorium.github.io/Mozzi/learn/introductory-tutorial/
   
@@ -14,20 +12,17 @@
     Audio output on digital pin 9 on a Uno or similar, or
     DAC/A14 on Teensy 3.1, or 
     check the README or http://sensorium.github.com/Mozzi/
-
   Potentiometer connected to analog pin 0:
      Center pin of the potentiometer goes to the analog pin.
      Side pins of the potentiometer go to +5V and ground
-
  +5V ---|
               /    
   A0 ----\  potentiometer 
               /    
  GND ---|
-
+ 
   Mozzi help/discussion/announcements:
   https://groups.google.com/forum/#!forum/mozzi-users
-
   Tim Barrass 2013, CC by-nc-sa.
   Modified Oct. 26 by Shea Kennedy
 */
@@ -36,13 +31,14 @@
 #include <MozziGuts.h>
 #include <Oscil.h> // oscillator template
 #include <tables/sin2048_int8.h> // sine table for oscillator
+
 //Include these for vibrato functionality
 #include <tables/cos2048_int8.h> // table for Oscils to play
 #include <mozzi_midi.h> // for mtof
 #include <mozzi_fixmath.h>
 
 //the control rate of the vibrato
-#define CONTROL_RATE 64 // powers of 2 please
+#define CONTROL_RATE 64 // must be a power of 2
 
 //for sine wave
 // use: Oscil <table_size, update_rate> oscilName (wavetable), look in .h file of table #included above
@@ -57,12 +53,12 @@ Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aSin(SIN2048_DATA);
 const char INPUT_PIN = 0; // set the input for the knob to analog pin 0
 const char INPUT_PIN1 = 1; // set the input for the knob to analog pin 1
 
-const byte volume = 100; // this is a multiplication factor to make the audio out louder
+const byte volume = 100; // this is a multiplication factor to make the audio out louder if higher or quiter if lower
 //byte intensity = 0; //vibrato intensity
 byte decimation = 0; //decimation intensity
 byte pitch = 0; //pitch intensity
 
-
+//This is our initializing statement/setup:
 void setup(){
   startMozzi(CONTROL_RATE);
 //  aSin.setFreq(1760);
@@ -70,7 +66,7 @@ void setup(){
   startMozzi(); // :))
 }
 
-
+//This updates what the value being read from the potentiometers is at any given time:
 void updateControl(){
   // read the variable resistor for volume
   int sensor_value = mozziAnalogRead(INPUT_PIN); // value is 0-1023
@@ -86,14 +82,14 @@ void updateControl(){
   //Serial.println((int)volume);
 }
 
-
+//This is the audio which is output from the synthesizer:
 int updateAudio(){
   //Q15n16 vibrato = (Q15n16) aVibrato.next();
   //return aCos.phMod(vibrato); // phase modulation to modulate frequency
   return ((int)aSin.next() * volume *decimation)>>8; // shift back into range after multiplying by 8 bit value
 }
 
-
+//This loops through updateAudio() automatically so changes to updateAudio() are heard instantly:
 void loop(){
   audioHook(); // required here
 }
